be7718b29e185e33851736550c4dc7a38a6be0a3
혁키 <dnwnsgur12@gmail.com>
2022-10-19 17:31:02 +0900

    main

--- app/src/main/java/com/project/siso/MainActivity.java
+++ app/src/main/java/com/project/siso/MainActivity.java
@@ -1,14 +1,42 @@
 package com.project.siso;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main_Activity";
+
+    private ImageView ivMenu;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivMenu = findViewById(R.id.iv_menu);
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //액션바 변경하기(들어갈 수 있는 타입 : Toolbar type
+        setSupportActionBar(toolbar);
+
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
     }
+
 }
