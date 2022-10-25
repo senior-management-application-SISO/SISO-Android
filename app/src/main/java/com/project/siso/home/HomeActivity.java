800e3a7f9df9a3ebb6f63ae68aa13d2aa9c9afb7
혁키 <dnwnsgur12@gmail.com>
2022-10-25 16:58:18 +0900

    home

---
+++ app/src/main/java/com/project/siso/home/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.project.siso.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.project.siso.R;
+import com.project.siso.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListeners();
+    }
+
+    //클릭 이벤트
+    private void setListeners() {
+        binding.signupButton.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+
+    }
+}
---
+++ app/src/main/java/com/project/siso/home/SignUpActivity.java
@@ -0,0 +1,29 @@
+package com.project.siso.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.project.siso.R;
+import com.project.siso.databinding.ActivityHomeBinding;
+import com.project.siso.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListeners();
+    }
+
+    //클릭 이벤트
+    private void setListeners() {
+        binding.cancelButton.setOnClickListener(v -> finish());
+
+    }
+}
