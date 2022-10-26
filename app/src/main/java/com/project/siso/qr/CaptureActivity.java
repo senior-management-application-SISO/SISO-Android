f80b44f2ec43ed6dc5f82856534f89fbf9fca4a4
혁키 <dnwnsgur12@gmail.com>
2022-10-26 12:42:59 +0900

    qr

---
+++ app/src/main/java/com/project/siso/qr/CaptureActivity.java
@@ -0,0 +1,2 @@
+package com.project.siso.qr;public class CaptureActivity {
+}
---
+++ app/src/main/java/com/project/siso/qr/QrCodeScanActivity.java
@@ -0,0 +1,14 @@
+package com.project.siso.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrCodeScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_scan);
+    }
+}
